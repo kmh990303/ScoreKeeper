984d90d571be37c917ba49075a95ea65efec57b2
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-05-06 20:30:20 +0900

    Update App.jsx

--- srcc/App.jsx
+++ srcc/App.jsx
@@ -1,34 +1,11 @@
 import './App.css'
-import TodoItem from './TodoItem';
-import TodoList from './TodoList';
-import CssBaseline from '@mui/material/CssBaseline';
-import ColorBoxGrid from './ColorBoxGrid'
-
-// const colors = [
-//   "#E53935",
-//   "#E91E63",
-//   "#9C27B0",
-//   "#673AB7",
-//   "#3F51B5",
-//   "#2196F3",
-//   "#03A9F4",
-//   "#00BCD4",
-//   "#009688",
-//   "#4CAF50",
-//   "#8BC34A",
-//   "#CDDC39",
-//   "#FFEB3B",
-//   "#FFC107",
-//   "#FF9800",
-//   "#FF5722",
-// ];
-
+import ScoreKeeper from './ScoreKeeper'
 
 
 function App() {
 
   return (
-    <ColorBoxGrid />
+    <ScoreKeeper />
   )
 }
 
